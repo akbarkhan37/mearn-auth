268c9ea23055dfd7ba3c8ccbfe8e843e6d3ab387
Akbar Khan <akbarkhan37@gmail.com>
2025-06-19 23:28:40 +0530

    header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <div className='bg-slate-200'>
-        <div className='container mx-auto p-4 flex justify-between items-center'>
-      <Link to='/'><h1 className='font-bold'>Auth App</h1></Link>
-      <ul>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/about'><li>About</li></Link>
-        <Link to='/signin'><li>Sign In</li></Link>
-      </ul>
+      <div className='container mx-auto p-4 flex justify-between items-center'>
+        <Link to='/'><h1 className='font-bold'>Auth App</h1></Link>
+        <ul className='flex gap-4'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/signin'>Sign In</Link></li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
